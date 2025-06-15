5fbaace19e9b55422d8091ede17c8b7f923ad6a9
Joao <arthursla878@gmail.com>
2025-06-15 17:58:29 -0300

    testeImagens

--- src/ocorrencia/login.jsx
+++ src/ocorrencia/login.jsx
@@ -11,10 +11,10 @@ export default function Login() {
                 <div className="one">
                     <h1 className="h1-one">Inicie no nosso site!</h1>
                     <p className="p-one">Faça seu login aqui professor, após esse processo você<br /> terá acesso a todas as nossas opções.</p>
-                    <a href="https://www.instagram.com/eeeppejoaoboscodelima/"><img src="../src/assets/instagram.svg" alt="" className="instagram-img" /></a>
+                    <a href="https://www.instagram.com/eeeppejoaoboscodelima/"><img src="../src/assets/main-index.png" alt="" className="instagram-img" /></a>
                 </div>
                 <div className="two">
-                    <a href=""><img src="../src/assets/login-two.png" alt="" className="login-img" /></a>
+                    <a href=""><img src="../src/assets/main-index.png" alt="" className="login-img" /></a>
                     <h1 className="h1-login">Login</h1>
                     <input type="email" placeholder="E-mail ou Matricula" className="input-two" />
                     <input type="password" placeholder="Senha" className="input-two" />
